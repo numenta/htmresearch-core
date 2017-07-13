b2255b88f6758261a5016021ac2799efbe2ddd0d
Luiz Scheinkman <lscheinkman@numenta.com>
2017-07-13 09:52:15 -0700

    Reuse nupic.core installation instead of rebuilding it

--- external/nupic_core.cmake
+++ external/nupic_core.cmake
@@ -22,7 +22,8 @@
 # Configure nupic_core and its third party dependencies
 #
 # INPUT VARIABLES: Passed via -DVAR=VALUE
-# 
+#    LOCAL_NUPIC_CORE_INSTALL_DIR: Use local nupic.core installed at this location
+#                                  instead of building
 #    LOCAL_NUPIC_CORE_SOURCE: If the LOCAL_NUPIC_CORE_SOURCE variable is set to
 #                             your local nupic_core sources, cmake will use your 
 #                             for the version specified in NUPIC_CORE_VERSION
@@ -34,15 +35,12 @@
 # OUPUT VARIABLES: Available to the parent scope
 #
 #   NUPIC_CORE_SOURCE_DIR: local or downloaded location of nupic_core sources
-#   NUPIC_CORE_BUILD_DIR: nupic_core build location
 #   NUPIC_CORE_INCLUDE_DIR: nupic_core include directory
 #   NUPIC_CORE_LIB_DIR: nupic_core libraries directory
 #   NUPIC_CORE_STATIC_LIB_TARGET: nupic_core static library target
 #   GTEST_STATIC_LIB_TARGET: gtest static library target installed by nupic_core
 #   SWIG_EXECUTABLE: swig executable installed by nupic_core
 #   SWIG_DIR: swig shared directory installed by nupic_core 
-#   APR1_STATIC_LIB_INC_DIR:  
-#   APRUTIL1_STATIC_LIB_INC_DIR:  
 # 
 # See CapnProto.cmake for other variables and functions  
 #
@@ -61,35 +59,48 @@ else(LOCAL_NUPIC_CORE_SOURCE)
     set(NUPIC_CORE_SOURCE_DIR "${EP_BASE}/Source/nupic_core")
 endif(LOCAL_NUPIC_CORE_SOURCE)
 
+# Use nupic.core local installation
+if(LOCAL_NUPIC_CORE_INSTALL_DIR)
+    add_custom_target(nupic_core)
+    set(NUPIC_CORE_INSTALL_DIR "${LOCAL_NUPIC_CORE_INSTALL_DIR}")
+else(LOCAL_NUPIC_CORE_INSTALL_DIR)
+    # Update file locations based on the external project location
+    set(NUPIC_CORE_BUILD_DIR "${EP_BASE}/Build/nupic_core")
+    set(NUPIC_CORE_INSTALL_DIR "${EP_BASE}/Install/nupic_core")
+    set(NUPIC_CORE_THIRDPARTY_DIR "${NUPIC_CORE_BUILD_DIR}/ThirdParty/Install")
 
-# Update file locations based on the external project location
-set(NUPIC_CORE_BUILD_DIR "${EP_BASE}/Build/nupic_core")
-set(NUPIC_CORE_INSTALL_DIR "${EP_BASE}/Install/nupic_core")
+    # Download and build nupic.core
+    set_directory_properties(PROPERTIES EP_BASE "${EP_BASE}")
+    ExternalProject_Add(
+        nupic_core
+        SOURCE_DIR ${LOCAL_NUPIC_CORE_SOURCE}
+        URL ${nupic_core_url}
+        UPDATE_COMMAND ""
+        PATCH_COMMAND ""
+        CMAKE_GENERATOR ${CMAKE_GENERATOR}
+        CMAKE_ARGS
+            ${CAPNP_CMAKE_DEFINITIONS}
+            -DNUPIC_BUILD_PYEXT_MODULES=OFF
+            -DCMAKE_INSTALL_PREFIX=${NUPIC_CORE_INSTALL_DIR}
+    )
+endif(LOCAL_NUPIC_CORE_INSTALL_DIR)
+
 set(NUPIC_CORE_INCLUDE_DIR "${NUPIC_CORE_INSTALL_DIR}/include")
 set(NUPIC_CORE_LIB_DIR "${NUPIC_CORE_INSTALL_DIR}/lib")
-set(NUPIC_CORE_THIRDPARTY_DIR "${NUPIC_CORE_BUILD_DIR}/ThirdParty/Install")
 
-# Download and build nupic.core
-set_directory_properties(PROPERTIES EP_BASE "${EP_BASE}")
-ExternalProject_Add(
-    nupic_core
-    SOURCE_DIR ${LOCAL_NUPIC_CORE_SOURCE}
-    URL ${nupic_core_url}
-    UPDATE_COMMAND ""
-    PATCH_COMMAND ""
-    CMAKE_GENERATOR ${CMAKE_GENERATOR}
-    CMAKE_ARGS
-        ${CAPNP_CMAKE_DEFINITIONS}
-        -DNUPIC_BUILD_PYEXT_MODULES=OFF
-        -DCMAKE_INSTALL_PREFIX=${NUPIC_CORE_INSTALL_DIR}
-)
+# Validate Nupic installation
+if(NOT EXISTS "${NUPIC_CORE_INCLUDE_DIR}/nupic")
+    message(FATAL_ERROR "Invalid nupic.core installation. \
+    Make sure nupic.core is installed the LOCAL_NUPIC_CORE_INSTALL_DIR variable \
+    points to a valid nupic.core installation")
+endif()
 
 # Expose nupic.core directories and targets
 set(NUPIC_CORE_SOURCE_DIR "${NUPIC_CORE_SOURCE_DIR}" PARENT_SCOPE)
-set(NUPIC_CORE_BUILD_DIR "${NUPIC_CORE_BUILD_DIR}" PARENT_SCOPE)
 set(NUPIC_CORE_INCLUDE_DIR "${NUPIC_CORE_INCLUDE_DIR}" PARENT_SCOPE)
 set(NUPIC_CORE_LIB_DIR "${NUPIC_CORE_LIB_DIR}" PARENT_SCOPE)
 set(NUPIC_CORE_STATIC_LIB_TARGET "${NUPIC_CORE_LIB_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}nupic_core${CMAKE_STATIC_LIBRARY_SUFFIX}" PARENT_SCOPE)
+set(NUPIC_CORE_INSTALL_DIR "${NUPIC_CORE_INSTALL_DIR}" PARENT_SCOPE)
 
 # Gtest 
 set(GTEST_STATIC_LIB_TARGET "${NUPIC_CORE_LIB_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX}gtest${CMAKE_STATIC_LIBRARY_SUFFIX}" PARENT_SCOPE)
@@ -97,14 +108,6 @@ set(GTEST_STATIC_LIB_TARGET "${NUPIC_CORE_LIB_DIR}/${CMAKE_STATIC_LIBRARY_PREFIX
 # Swig
 set(SWIG_EXECUTABLE "${NUPIC_CORE_INSTALL_DIR}/bin/swig${CMAKE_EXECUTABLE_SUFFIX}" PARENT_SCOPE)
 set(SWIG_DIR "${NUPIC_CORE_INSTALL_DIR}/share/swig/3.0.2" PARENT_SCOPE)
-
-# Apr1
-set(APR1_STATIC_LIB_INC_DIR "${NUPIC_CORE_THIRDPARTY_DIR}/Apr1StaticLib/include")
-set(APR1_STATIC_LIB_INC_DIR "${NUPIC_CORE_THIRDPARTY_DIR}/Apr1StaticLib/include" PARENT_SCOPE)
-
-# Aprutil1
-set(APRUTIL1_STATIC_LIB_INC_DIR "${NUPIC_CORE_THIRDPARTY_DIR}/AprUtil1StaticLib/include")
-set(APRUTIL1_STATIC_LIB_INC_DIR "${NUPIC_CORE_THIRDPARTY_DIR}/AprUtil1StaticLib/include" PARENT_SCOPE)
 
 # Capnp
 include(CapnProto)
